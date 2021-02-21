494a88b575c9e205eb605a230c703cd7156ef757
bachirLadj <fb_ladjouzi@esi.dz>
2021-02-21 11:43:22 +0100

    add

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by bachirladj on 21/2/21.
+ */
+
+'use strict';
+
+import ViewPager from './viewpager/ViewPager';
+import IndicatorViewPager from './viewpager/IndicatorViewPager';
+import PagerDotIndicator from './viewpager/indicator/PagerDotIndicator';
+import PagerTitleIndicator from './viewpager/indicator/PagerTitleIndicator';
+import PagerTabIndicator from './viewpager/indicator/PagerTabIndicator';
+
+export {
+    ViewPager,
+    IndicatorViewPager,
+    PagerDotIndicator,
+    PagerTitleIndicator,
+    PagerTabIndicator
+};
